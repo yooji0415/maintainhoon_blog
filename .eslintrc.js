301457f8a4be64bf08e8deb7cd77b28b846113d0
yooji0415 <gch05406@gmail.com>
2023-01-22 16:05:28 +0900

    chore: eslint 관련 에러 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,28 +6,29 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended", // 해당 플러그인의 권장 규칙을 사용합니다.
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended', // 해당 플러그인의 권장 규칙을 사용합니다.
+    'plugin:prettier/recommended',
+    'next',
   ],
-  parser: "@typescript-eslint/parser", // ESLint 파서를 지정합니다.
+  parser: '@typescript-eslint/parser', // ESLint 파서를 지정합니다.
   parserOptions: {
     ecmaFeatures: {
       jsx: true, // JSX를 파싱할 수 있습니다.
     },
     ecmaVersion: 12, // Modern ECMAScript를 파싱할 수 있습니다.
-    sourceType: "module", // import, export를 사용할 수 있습니다.
+    sourceType: 'module', // import, export를 사용할 수 있습니다.
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     // ESLint 규칙을 지정합니다. extends에서 지정된 규칙을 덮어 쓸수도 있습니다.
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
   settings: {
     react: {
-      version: "detect", // 현재 사용하고 있는 react 버전을 eslint-plugin-react가 자동으로 감지합니다.
+      version: 'detect', // 현재 사용하고 있는 react 버전을 eslint-plugin-react가 자동으로 감지합니다.
     },
   },
 };
