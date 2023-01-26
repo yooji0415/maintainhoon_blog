b8e979932007e4b3aa53de2f9ab875ffd257d680
yooji0415 <gch05406@gmail.com>
2023-01-26 16:09:38 +0900

    refactor: 구글 사이트 콘솔 등록 시도

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="DRYUx28iQjhdhrHPnvTqyrV522Tx2i4wUm53wfft6_U"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
