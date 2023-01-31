4fdae9ec90b24462b45cafc890fbbf93ca7b0137
yooji0415 <gch05406@gmail.com>
2023-01-31 21:55:05 +0900

    refactor: 에러 페이지나 검색시 화면 하단이 비어있는 경우를 제거

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout(props: LayoutProps) {
   return (
     <>
       <Header />
-      <main className="transition duration-500 bg-white dark:bg-[#111111] text-black dark:text-white">
+      <main className="transition duration-500 min-h-screen bg-white dark:bg-[#111111] text-black dark:text-white">
         <div className="max-w-screen-md flex flex-col px-10 m-auto">{props.children}</div>
       </main>
       <Footer />
