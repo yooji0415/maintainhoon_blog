d8365c6cc705e66eda1302a1044acdb372c5f073
yooji0415 <gch05406@gmail.com>
2023-02-01 03:24:10 +0900

    feat: 카테고리 페이지 생성을 위한 데이터 수정 및 생성

---
+++ data/categorys.ts
@@ -0,0 +1,9 @@
+export const categorys: { title: string; keyword: string }[] = [
+  { title: '🐱 All', keyword: '' },
+  { title: '📖 JavaScript', keyword: 'javascript' },
+  { title: '🧑🏻‍💻 회고', keyword: 'retrospection' },
+  { title: '🖥 Web', keyword: 'web' },
+  { title: '👨🏻‍🏫 React', keyword: 'react' },
+  { title: '📝 정리', keyword: 'arrangement' },
+  { title: '🎸 기타', keyword: 'other' },
+];
--- data/nav.ts
+++ data/nav.ts
@@ -1,4 +1,5 @@
 export const nav: { title: string; location: string }[] = [
   { title: 'Home', location: '/' },
   { title: 'Blog', location: '/blog' },
+  { title: 'Category', location: '/category' },
 ];
