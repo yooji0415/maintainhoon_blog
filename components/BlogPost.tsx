b3e113cf31cb13823f4c449bca9d044cd8a97c42
yooji0415 <gch05406@gmail.com>
2023-02-01 13:18:42 +0900

    refactor: 게시글 제목 다크모드 변경시 딜레이 수정

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -11,7 +11,7 @@ const BlogPost = ({ date, title, des, slug }: BlogPostProps) => {
   return (
     <Link href={`/blog/post/${slug}`} passHref className="w-full my-7">
       <div className="font-medium text-xs transition text-gray-500 dark:text-gray-300">{date}</div>
-      <div className="font-extrabold text-xl sm:text-2xl mt-2 transition hover:text-green-500">
+      <div className="font-extrabold text-xl sm:text-2xl mt-2 transition text-black dark:text-white hover:text-green-500 dark:hover:text-green-500">
         {title}
       </div>
       <div className="font-medium text-lg transition text-gray-600 dark:text-gray-400 sm:text-xl mt-1">
